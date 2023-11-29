48a610a082ba8de59d452a37d78ac8de54b4bcd5
adambecker <adambeckercodes@gmail.com>
2023-11-29 10:39:45 +0600

    add javascript for hints

--- js/main.js
+++ js/main.js
@@ -1,15 +1,31 @@
-const infoBtns = document.querySelectorAll(".info-dot");
+const infoBtns = document.querySelectorAll('.info-dot');
+const infoHints = document.querySelectorAll('.info-hint');
 
 // Клик по кнопкам с подсказками
-
 for (let btn of infoBtns) {
-  btn.addEventListener('click', showHint);
+	btn.addEventListener('click', function (e) {
+		e.stopPropagation();
+
+		// Hide all hint
+		for (let hint of infoHints) {
+			if (this.parentNode.querySelector('.info-hint') !== hint) {
+				hint.classList.add('none');
+			}
+		}
+
+		// Show current hint
+		this.parentNode.querySelector('.info-hint').classList.toggle('none');
+	});
 }
 
-function showHint () {
-  this.parentNode.querySelector(".info-hint").classList.toggle("none");
+// Закрываем подсказки при клике по всему документу
+document.addEventListener('click', function () {
+	for (let hint of infoHints) {
+		hint.classList.add('none');
+	}
+});
+
+// Запрещаем всплытие события клика при клике на подсказки
+for (let hint of infoHints) {
+	hint.addEventListener('click', (e) => e.stopPropagation());
 }
-
-// Закрываем подсказки по клику по всему документу
-
-document.addEventListener
